b752db336158a81151f549d90012c3094ea6d26a
BachoKartsivadze <bachoqarcivadze62@gmail.com>
2024-06-11 15:03:30 +0400

    added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { ListBarComponent } from './list-bar/list-bar.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
